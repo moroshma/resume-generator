b8cff6100d7543c8e72c6a423898e13d5b17a206
Krechun Artem <krechun.artem@wb.ru>
2025-04-03 00:58:32 +0300

    add interface

--- resume_storage/internal/resume/models/resume.go
+++ resume_storage/internal/resume/models/resume.go
@@ -22,3 +22,25 @@ type ResumeInfo struct {
 	CreatedAt time.Time `json:"created_at"`
 	Title     string    `json:"title"`
 }
+
+type ResumeRepositoryI interface {
+	CreateResume(resume Resume) (uint, error)
+	DeleteResumeByID(resumeID uint) error
+	GetResumeInfoByID(resumeID uint) (ResumeInfo, error)
+	GetAllResumesPreview() ([]ResumeInfo, error)
+	GetResumeByID(resumeID uint) (Resume, error)
+}
+
+type ResumeUseCaseI interface {
+	CreateResume(resume Resume) (uint, error)
+	DeleteResumeByID(resumeID uint) error
+	GetResumeInfoByID(resumeID uint) (ResumeInfo, error)
+	GetAllResumesPreview() ([]ResumeInfo, error)
+	GetResumeByID(resumeID uint) (Resume, error)
+}
+
+type ResumePdfStorageI interface {
+	UploadFile(object Resume) (string, error)
+	DownloadFile(objectName string) (Resume, error)
+	DeleteFile(userID, payloadID uint) (string, error)
+}
