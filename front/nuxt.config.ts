ea64a094b1163d245d4e034a096814c16d7ad9a8
Егор Золовкин <kerminegor@gmail.com>
2025-04-15 14:05:43 +0300

    docker + proxy traefik

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -24,4 +24,13 @@ export default defineNuxtConfig({
       viewport: "width=device-width, initial-scale=1, maximum-scale=1",
     },
   },
+  nitro: {
+    routeRules: {
+      "/api/**": {
+        proxy: {
+          to: "http://traefik:80/**",
+        },
+      },
+    },
+  },
 });
