95a55e26414ff3c9cc2483e2ae480e3c139279cb
Egor <kerminegor@gmail.com>
2025-05-03 01:43:22 +0300

    fix encode bug

--- front/server/api/resume/pdf/create.post.ts
+++ front/server/api/resume/pdf/create.post.ts
@@ -1,17 +1,44 @@
+import { defineEventHandler, readBody, createError } from "h3";
+import { $fetch } from "ofetch";
+
 export default defineEventHandler(async (event) => {
-  const { BASE_HOST } = useRuntimeConfig();
-  const body = await readBody(event);
+  const runtimeConfig = useRuntimeConfig();
+  const BASE_HOST = runtimeConfig.BASE_HOST;
 
-  const response = await $fetch.raw(
-    `${BASE_HOST}/resume_storage/api/v001/users/resume`,
-    {
-      method: "POST",
-      body,
-      headers: {
-        ...(event.context.cookies && { cookie: event.context.cookies }),
-      },
+  const body = await readRawBody(event, false);
+
+  const clientHeaders = event.node.req.headers;
+
+  try {
+    const response = await $fetch.raw(
+      `${BASE_HOST}/resume_storage/api/v001/users/resume`,
+      {
+        method: "POST",
+        body: body,
+        headers: {
+          ...(event.context.cookies && { cookie: event.context.cookies }),
+          "content-type": clientHeaders["content-type"],
+        },
+      }
+    );
+
+    return response._data;
+  } catch (error: any) {
+    console.error("Ошибка при запросе к бэкенду:", error.message);
+    if (error.response) {
+      console.error("Backend Status:", error.response.status);
+      console.error("Backend Status Text:", error.response.statusText);
+      console.error("Backend Response Data:", error.data);
+    } else {
+      console.error("Error details:", error);
     }
-  );
 
-  return response._data;
+    throw createError({
+      statusCode: error.response?.status || 500,
+      statusMessage: `Backend Error: ${
+        error.response?.statusText || error.message
+      }`,
+      data: error.data || { message: error.message },
+    });
+  }
 });
