9a3d48e6ac41939b7f54fe2ec44793d94eda0c44
Egor <kerminegor@gmail.com>
2025-05-03 12:19:23 +0300

    refresh handler

--- front/server/api/resume/pdf/create.post.ts
+++ front/server/api/resume/pdf/create.post.ts
@@ -9,36 +9,16 @@ export default defineEventHandler(async (event) => {
 
   const clientHeaders = event.node.req.headers;
 
-  try {
-    const response = await $fetch.raw(
-      `${BASE_HOST}/resume_storage/api/v001/users/resume`,
-      {
-        method: "POST",
-        body: body,
-        headers: {
-          ...(event.context.cookies && { cookie: event.context.cookies }),
-          "content-type": clientHeaders["content-type"],
-        },
-      }
-    );
-
-    return response._data;
-  } catch (error: any) {
-    console.error("Ошибка при запросе к бэкенду:", error.message);
-    if (error.response) {
-      console.error("Backend Status:", error.response.status);
-      console.error("Backend Status Text:", error.response.statusText);
-      console.error("Backend Response Data:", error.data);
-    } else {
-      console.error("Error details:", error);
-    }
-
-    throw createError({
-      statusCode: error.response?.status || 500,
-      statusMessage: `Backend Error: ${
-        error.response?.statusText || error.message
-      }`,
-      data: error.data || { message: error.message },
-    });
-  }
+  return createApiHandler({
+    url: `/resume_storage/api/v001/users/resume`,
+    event,
+    options: {
+      method: "POST",
+      body,
+      headers: {
+        ...(event.context.cookies && { cookie: event.context.cookies }),
+        "content-type": clientHeaders["content-type"],
+      },
+    },
+  });
 });
