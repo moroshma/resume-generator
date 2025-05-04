5b2fa495bad67558aa86ebe67e7c7501c861e4ed
Egor <kerminegor@gmail.com>
2025-05-04 23:29:53 +0300

    valid date message

--- front/utils/errors/ValidationError.ts
+++ front/utils/errors/ValidationError.ts
@@ -1,5 +1,8 @@
 export class ValidationError extends Error {
-  constructor(message = "bar", ...params: any) {
+  constructor(
+    message = "Не заполнены или неправильно заполнены поля",
+    ...params: any
+  ) {
     super(message, ...params);
 
     if (Error.captureStackTrace) {
