17b0e40e1955e2c1f6f06a5ee09cac1477a23c27
Krechun Artem <krechun.artem@wb.ru>
2025-06-30 20:42:57 +0300

    upd

--- resume_storage/cmd/main.go
+++ resume_storage/cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/moroshma/resume-generator/resume_storage/internal/app"
+	"net"
 )
 
 func main() {
 	app.Run()
 
+	ips, err := net.LookupHost("resume-storage")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("replicas: %v\n", ips)
 }
